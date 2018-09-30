b72e73ac08cad4ff4e857cd5e479224314248d0b
Yun Teng <yteng@thoughtworks.com>
2018-09-30 08:48:48 +0800

    lesson-1:added babel

--- lesson-1/src/index.js
+++ lesson-1/src/index.js
@@ -1,5 +1,7 @@
 import _ from 'lodash'
 
+let result = [1, 2, 3].map((n) => n + 1);
+
 let element = document.createElement('div');
-element.innerHTML = _.join(['Hello', 'World'], ' ')
+element.innerHTML = _.join(result, '-');
 document.body.appendChild(element);
--- lesson-1/webpack.config.js
+++ lesson-1/webpack.config.js
@@ -1,9 +1,26 @@
 const path = require('path');
 
+
+function resolve(dir) {
+    return path.join(__dirname, '..', dir)
+}
+
+
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath:"dist"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: [resolve('src')],
+                exclude: [resolve('node_modules')]
+            }
+        ]
     }
 };
