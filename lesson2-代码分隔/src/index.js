b40d68f7b0e15a25215c4196438cb4f539d9ef7f
Yun Teng <yteng@thoughtworks.com>
2018-10-01 22:17:07 +0800

    dynamic import for split

--- lesson2-代码分隔/src/index.js
+++ lesson2-代码分隔/src/index.js
@@ -1,6 +1,6 @@
-import  'babel-polyfill'
-import _ from 'lodash'
-import axios from 'axios'
+import "babel-polyfill";
+import _ from "lodash";
+import axios from "axios";
 
 const request = axios.create({
     timeout: 10 * 1000,
@@ -13,3 +13,15 @@ let result = [1, 2, 3].map((n) => n + 1);
 let element = document.createElement('div');
 element.innerHTML = _.join(result, '-');
 document.body.appendChild(element);
+
+var button = document.createElement('button');
+button.innerHTML = 'Say hello';
+document.body.appendChild(button);
+button.onclick = e =>
+    import(/* webpackChunkName: "hello" */ './hello').then(module => {
+        var hello = module.default;
+
+        hello();
+    });
+
+
