ae8130b341ec1705e67fa6772c118021e9e71361
Yun Teng <yteng@thoughtworks.com>
2018-10-12 23:42:21 +0800

    lesson5:split js code

--- lesson5-分离production和development环境/webpack.prod.conf.js
+++ lesson5-分离production和development环境/webpack.prod.conf.js
@@ -1,10 +1,30 @@
 const merge = require('webpack-merge');
+const webpack = require('webpack');
 const baseWebpackConfig = require('./webpack.base.conf');
 
 const webpackConfig = merge(baseWebpackConfig, {
     //environment specific config goes here
     mode: 'production',
-    devtool: 'source-map'
+    devtool: 'source-map',
+    optimization: {
+        runtimeChunk: {
+            "name": "manifest"
+        },
+        splitChunks: {
+            cacheGroups: {
+                default: false,
+                vendors: false,
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                }
+            }
+        }
+    },
+    plugins: [
+        new webpack.HashedModuleIdsPlugin()
+    ]
 });
 
 module.exports = webpackConfig;
